6957c3e098bbd7d705cac86394d7fc9568b31533
Samuel Akinboro <akinboroo@gmail..com>
2024-04-06 22:03:56 +0100

    fixed pattern

--- js/script.js
+++ js/script.js
@@ -6,31 +6,31 @@ menuToggle.addEventListener("click", () => {
   showcase.classList.toggle("active");
 });
 
-$(document).ready(function () {
-    $(".customer-logos").slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 4,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 3,
-          },
-        },
-      ],
-    });
-  });
+// $(document).ready(function () {
+//     $(".customer-logos").slick({
+//       slidesToShow: 6,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//       autoplaySpeed: 1500,
+//       arrows: false,
+//       dots: false,
+//       pauseOnHover: false,
+//       responsive: [
+//         {
+//           breakpoint: 768,
+//           settings: {
+//             slidesToShow: 4,
+//           },
+//         },
+//         {
+//           breakpoint: 480,
+//           settings: {
+//             slidesToShow: 3,
+//           },
+//         },
+//       ],
+//     });
+//   });
 
   document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
